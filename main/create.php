ff1f147c03497333d0fcf0a2bf4d845e98bff071
BuildTools <unconfigured@null.spigotmc.org>
2021-11-20 21:10:56 +0800

    first version with search book and make a new book

---
+++ main/create.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    $bookName=$_POST["bookName"];
+    $author=$_POST["author"];
+    $ISBN=$_POST["ISBN"];
+    $describeBook=$_POST["describeBook"];
+    echo "bookName是 $bookName<br>";
+    echo "author是 $author<br>";
+    echo "ISBN是 $ISBN<br>";
+    echo "describeBook是 $describeBook<br>";
+    $host = 'localhost';
+    $dbuser ='root';
+    $dbpassword = '123456';
+    $dbname = 'test';
+    $link = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
+    if($link){
+        mysqli_query($link,'SET NAMES uff8');
+        echo "正確連接資料庫";
+    }
+    else {
+        echo "不正確連接資料庫</br>" . mysqli_connect_error();
+    }
+    $sql="insert into `book` (`bookName`,`author`,`ISBN`,`describeBook`)VALUE('$bookName','$author','$ISBN','$describeBook')";
+    $result = mysqli_query($link,$sql);
+    if (mysqli_affected_rows($link)>0) {
+        // 如果有一筆以上代表有更新
+        // mysqli_insert_id可以抓到第一筆的id
+        $new_id= mysqli_insert_id ($link);
+        echo "資料已更新";
+        }
+        elseif(mysqli_affected_rows($link)==0) {
+            echo "無資料新增";
+        }
+        else {
+            echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
+        }
+         mysqli_close($link); 
+          
+         header('Location: select.php');
+    ?>
+</body>
+</html>
---
+++ main/index.php
@@ -0,0 +1,34 @@
+<html xmlns=”http://www.w3.org/1999/xhtml”>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	</head>
+  <style>
+    body{
+      
+      text-align:center;
+    }
+    button{
+      width:300px;
+      height:300px;
+      font-size:50px;
+      box-sizing:border-box;
+      margin-top:10%;
+    }
+    </style>
+<body>
+<?php
+
+?>
+<table>
+  <tr>
+ <a href="select.php"><button>查詢</button></a> 
+ <a href="insert.php"><button>新增書籍</button></a>
+  </tr>
+ <!-- <li><a href="update.php">UPDATE，更新資料表中的資料</a></li>
+ <li><a href="delete.php">DELETE，刪除資料表中的資料</a></li> -->
+ 
+
+
+</body>
+
+</html>
