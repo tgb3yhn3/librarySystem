c1750f2c7b67ffe8c569e3b5ecb170bd20e174f4
warren00857020 <warren5606@gmail.com>
2021-12-10 21:59:10 +0800

    12

--- create_condition.php
+++ create_condition.php
@@ -1,3 +1,6 @@
 <?php
-    
+    function create_condition($userID){
+        $conn=require_once("config.php");
+        $sql="INSERT INTO user_condition (userID,book_num, book_time, book_fine) VALUES($userID,,)";
+    }
 ?>
