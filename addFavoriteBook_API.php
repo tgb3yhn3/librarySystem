8e98cd89c424bae2052127340b0fc8d45a3daec8
wys899195 <73873365+wys899195@users.noreply.github.com>
2021-12-14 09:57:25 +0800

    使用者將書籍加入我的最愛後端完成大部分
    使用者將書籍加入我的最愛後端完成大部分!

---
+++ addFavoriteBook_API.php
@@ -0,0 +1,30 @@
+<?php//addFavoriteBooK.php
+
+/*要新增到最愛的書籍之資料*/
+$userID   = $_POST["userID"];
+$bookName = $_POST["bookName"];
+$author   = $_POST["author"];
+$ISBN     = $_POST["ISBN"];
+
+/*與資料庫連線*/
+$conn=require_once "config.php";
+          
+
+/*使用者將書籍加入最愛的紀錄存進資料庫中*/
+$sql="insert into user_favorite_book_data(userID,bookName,author,ISBN) values('$userID','$bookName','$author','$ISBN')";
+mysqli_query($link,$sql);
+mysqli_close($link);//關閉資料庫連線
+echo "<script>alert('加入最愛成功!');history.back();</script>";
+
+    /*資料庫table
+
+    CREATE TABLE `user_favorite_book_data` (
+    `userID` varchar(10) COLLATE utf8_croatian_ci NOT NULL,
+    `ISBN` varchar(20) COLLATE utf8_croatian_ci NOT NULL,
+    `author` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+    `bookName` varchar(100) COLLATE utf8mb4_croatian_ci DEFAULT NULL,
+    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+    
+    */
+?>
