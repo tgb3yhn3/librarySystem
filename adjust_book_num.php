f035697c23c0faa803536e1d76dd38788ac6dd06
warren00857020 <warren5606@gmail.com>
2021-12-18 00:42:27 +0800

    ibiuhqwg

--- adjust_book_num.php
+++ adjust_book_num.php
@@ -1,7 +1,7 @@
 <?php
     function adjust_book_status($bookuniqueID,$number){
         $conn=require_once("config.php");
-        $adjust_status = 'UPDATE book SET status = "'.$number.'" WHERE bookUniqueID = $bookuniqueID';
+        $adjust_status = 'UPDATE book SET status = "'.$number.'" WHERE bookUniqueID = "'.$bookuniqueID.'"';
         mysqli_query($conn,$adjust_status);
     }
 ?>
