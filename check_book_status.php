fd7db83fce0a8bdbbd65b0344273ad4b4eef8e31
warren00857020 <warren5606@gmail.com>
2021-12-21 19:07:26 +0800

    1221 return_book check

--- check_book_status.php
+++ check_book_status.php
@@ -1,8 +1,7 @@
 <?php
-    function check_user_renting_book($userID,$bookuniqueID,$conn){
-        $get_user_credit =  "SELECT * FROM user_book_history WHERE userID = $userID AND book_status = '出借中'";
-        $result = mysqli_query($conn,$get_user_credit);
-        $count=0;
+    function check_book_status($bookuniqueID,$conn){
+        $check_status = "SELECT * FROM book WHERE bookUniqueID = '$bookuniqueID'";
+        $result = mysqli_query($conn,$check_status);
         if ($result) {
             // mysqli_num_rows方法可以回傳我們結果總共有幾筆資料
             if (mysqli_num_rows($result)>0) {
@@ -12,17 +11,16 @@
                 while ($row = mysqli_fetch_assoc($result)) {
                     // 每跑一次迴圈就抓一筆值，最後放進data陣列中
                     $datas[] = $row;
-                    $count++;
                 }
             }
             // 釋放資料庫查到的記憶體
             mysqli_free_result($result);
         }
-        for($i = 0;$i<$count;$i++){
-            if($datas[$i]["book_unique_ID"] == $bookuniqueID){
-                return true;
-            }
+        if($datas[0]["status"]==0){
+            return true;
         }
-        return false;
+        else{
+            return false;
+        }
     }
 ?>
--- rent_book.php
+++ rent_book.php
@@ -3,14 +3,20 @@
     require_once('adjust_book_status.php');
     require_once('adjust_user_condition.php');
     require_once('push_book_history.php');
+    require_once('check_book_status.php');
     $conn=require_once("config.php");
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         $userID=$_POST["userID"];
         $bookuniqueID=$_POST["bookuniqueID"];
         if(check_condition($userID,$conn)){
-            adjust_book_status($bookuniqueID,1,$conn);
-            adjust_user_condition($userID,1,$conn);
-            push_book_history($userID,$bookuniqueID,$conn);
+            if(check_book_status($bookuniqueID,$conn)){
+                adjust_book_status($bookuniqueID,1,$conn);
+                adjust_user_condition($userID,1,$conn);
+                push_book_history($userID,$bookuniqueID,$conn);
+            }
+            else{
+                echo "此書已被借走";
+            }
         }
         else{
             echo "已達借書上限,不得借書";
