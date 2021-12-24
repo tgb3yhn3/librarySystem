5f29446a59a4b9cebaee0a6f93ce16fa7a524aec
warren00857020 <warren5606@gmail.com>
2021-12-24 19:56:11 +0800

    1224 0756

--- rent_book.php
+++ rent_book.php
@@ -16,20 +16,25 @@
             $ISBN = $ISBN.$array[$i];
         }
         if(check_reserve($userID,$ISBN,$conn)){//看是不是已被預約的書
-            echo "有預約";
+            echo "有預約<br>";
             adjust_book_status($bookuniqueID,1,$conn);//把此書登記為借出
             adjust_book_history_reserve($userID,$bookuniqueID,$ISBN,$conn);//把history已預約的狀態改成出借中
         }
         else{
             if(check_condition($userID,$conn)){//查user還有沒有扣打
-                if(check_book_status($bookuniqueID,$conn)){//查此書有沒有被借走(理論上用不到)
-                    echo "正常借書";
-                    adjust_book_status($bookuniqueID,1,$conn);//把此書登記為借出
-                    adjust_user_condition($userID,1,$conn);//user的renting_book_num+1
-                    push_book_history($userID,$bookuniqueID,$conn);//將借書資訊放到history
+                if(check_late_return($userID,$conn)){//查user有沒有逾期還書
+                    if(check_book_status($bookuniqueID,$conn)){//查此書有沒有被借走(理論上用不到)
+                        echo "正常借書<br>";
+                        adjust_book_status($bookuniqueID,1,$conn);//把此書登記為借出
+                        adjust_user_condition($userID,1,$conn);//user的renting_book_num+1
+                        push_book_history($userID,$bookuniqueID,$conn);//將借書資訊放到history
+                    }
+                    else{
+                        echo "此書已被借走";
+                    }
                 }
                 else{
-                    echo "此書已被借走";
+                    echo "有逾期書籍未歸還";
                 }
             }
             else{
