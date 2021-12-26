34ff0c8e8725574ef66c72e42f33667c3ac2cd1c
T.Y Kuan <tingyoungkuan@gmail.com>
2021-12-26 15:48:29 +0800

    del

--- adjust_book_status.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function adjust_book_status($bookuniqueID,$number,$conn){
-        $adjust_status = "UPDATE book SET status = '".$number."' WHERE bookUniqueID = '".$bookuniqueID."'";
-        mysqli_query($conn,$adjust_status);
-    }
-?>
