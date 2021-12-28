fc18f29c5a20a71877ec02a6556ec62cc30fcf55
T.Y Kuan <tingyoungkuan@gmail.com>
2021-12-28 16:06:27 +0800

    Home的href連結到首頁

--- main/php-book/borrow_history.php
+++ main/php-book/borrow_history.php
@@ -116,7 +116,7 @@
 
 
             <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+                <li class="nav-item"><a href="../index.php" class="nav-link px-2 text-muted">Home</a></li>
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
             </ul>
         </footer>
