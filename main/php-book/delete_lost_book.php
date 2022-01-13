c197bb3dbe5112119f4bd440a457f96101814cb2
roger00857002 <73873468+roger00857002@users.noreply.github.com>
2022-01-14 00:27:57 +0800

    匯出格式修改

--- main/php-book/delete_lost_book.php
+++ main/php-book/delete_lost_book.php
@@ -42,9 +42,16 @@
 <html lang="en">
 
     <head>
-        <meta charset="UTF-8">
+    <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <!-- Bootstrap CSS -->
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+	      <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+	      <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
         <title>HTML2PDF</title>
         <script src="https://raw.githack.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js" defer></script>
         <script src="./script.js" defer></script>
